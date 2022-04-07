c945c37c12cbebcdcdee841d948fc6a6269b442f
Fede180600 <fede180600@libero.it>
2022-04-07 15:46:55 +0200

    pseudo-code

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//Chiedere all'utente con due prompt quanti km deve percorrere e quanti anni ha il passeggero.
+
+//Definire il prezzo della tratta (0.21 € al km)
+
+//Se il passeggero è minorenne applicare il 20% di sconto sul prezzo della tratta
+//Se il passeggero è over 65 applicare il 40% di sconto sul prezzo della tratta
+
+//Impostare l'output del prezzo finale con massimo due decimali
+
+//Stampare l'output nell'html
