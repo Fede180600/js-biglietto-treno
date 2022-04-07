f3c8895a7c1ab082726ca410a9094473c54a918d
Fede180600 <fede180600@libero.it>
2022-04-07 15:48:06 +0200

    add-prompts

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 //Chiedere all'utente con due prompt quanti km deve percorrere e quanti anni ha il passeggero.
 
+const userKm = parseInt(prompt("Quanti Km devi percorrere?"));
+console.log(userKm, typeof(userKm));
+
+const userAge = parseInt(prompt("Quanti anni hai?"));
+console.log(userAge, typeof(userAge));
+
 //Definire il prezzo della tratta (0.21 € al km)
 
 //Se il passeggero è minorenne applicare il 20% di sconto sul prezzo della tratta
