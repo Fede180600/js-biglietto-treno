ba6f42ca4b10101cab2904fb5ac5e339df0feee5
Fede180600 <fede180600@libero.it>
2022-04-07 16:16:40 +0200

    add-discounted-price-calc

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 const userKm = parseInt(prompt("Quanti Km devi percorrere?"));
 console.log(userKm, typeof(userKm));
 
-const userAge = parseInt(prompt("Quanti anni hai?"));
+const userAge = parseInt(prompt("Quanti anni ha il passeggero?"));
 console.log(userAge, typeof(userAge));
 
 //Definire il prezzo della tratta (0.21 € al km)
@@ -11,14 +11,14 @@ const userPrice = 0.21 * userKm
 console.log(userPrice, typeof(userPrice));
 //Se il passeggero è minorenne applicare il 20% di sconto sul prezzo della tratta
 //Se il passeggero è over 65 applicare il 40% di sconto sul prezzo della tratta
-let discount
+let userPriceDiscounted = userPrice;
 	
 	if (18 > userAge) {
-		const userPriceDiscount1 = (userPrice * 20) / 100;
-		console.log(userPriceDiscount1)
+		const userPriceDiscounted = userPrice -(userPrice * 20) / 100;
+		console.log(userPriceDiscounted)
 	} else if (userAge > 65) {
-		const userPriceDiscount2 = (userPrice * 40) / 100;
-		console.log(userPriceDiscount2)
+		const userPriceDiscounted = userPrice - (userPrice * 40) / 100;
+		console.log(userPriceDiscounted)
 	}
 
 //Impostare l'output del prezzo finale con massimo due decimali
